c7465003cdae90ad18d27f05ed407667819240e0
jessicafg <je.a.ruiva@gmail.com>
2022-09-09 21:46:28 -0300

    fix formulario.py

--- formulario.py
+++ formulario.py
@@ -1,43 +1,42 @@
 import os
-from flask import Flask, request, abort, render_template
-from Flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app = Flask(_name_)
+app = Flask(__name__)
 
-#mySQL configurations
+# MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYQSL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYQSL_DATABASE_DB'] = 'teste'
-app.config['MYQSL_DATABASE_HOST'] = '172.17.0.2'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 @app.route('/')
-def mains():
+def main():
     return render_template('formulario.html')
 
-
-@app.route('/gravar', methods=['POST', GET])
+@app.route('/gravar', methods=['POST','GET'])
 def gravar():
-    nome = request.form['nome'] 
-    cpf = request.form['cpf'] 
-    endereco = request.form['endereco'] 
-    if nome and cpf and endereco:
-        conn = mysql.connect() 
-        cursor = conn.cursor() 
-        cursor.execute('insert int formulario_mvc (user_name, number_cpf, user_adress) VALUES (%s, %s, %s)', (nome,cpf,endereco))
-        conn.comit() 
-    return render_template('formulario.html') 
+  nome = request.form['nome']
+  cpf = request.form['cpf']
+  endereco = request.form['endereco']
+  if nome and cpf and endereco:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into tbl_user (user_name, user_cpf, user_endereco) VALUES (%s, %s, %s)', (nome, cpf, endereco))
+    conn.commit()
+  return render_template('formulario.html')
+
 
 @app.route('/listar', methods=['POST','GET'])
 def listar():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('select user_name, user_username, user_password from tbl_user')
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('lista.html', datas=data)
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute('select user_name, user_cpf, user_endereco) from tbl_user')
+  data = cursor.fetchall()
+  conn.commit()
+  return render_template('lista.html', datas=data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5008))
